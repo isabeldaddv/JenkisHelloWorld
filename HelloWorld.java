e22675d60777f5dc8f868f08a46733488ed27bf4
Isabel de Aguilar <Isabel.DeAguilar@solera.com>
2022-11-02 13:31:34 +0100

    Adding new files

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("--****Hello World and welcome to Jenkins training****---");
+	}
+}
---
+++ module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Isabel.DeAguilar
+ *
+ */
+module HelloWorld {
+}
